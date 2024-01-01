441f5d651defa20fbb2be0fdb4ec2a6066e68e3c
Showkat Ali <71689825+Showkiip@users.noreply.github.com>
2024-01-01 17:56:27 +0500

    asd

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,8 @@
 import { notFound } from "next/navigation";
-
+function getRandomInt(count:number)
+{
+  return Math.floor(Math.random()*count);
+}
 export default function ReviewDetails({
   params,
 }: {
@@ -8,6 +11,11 @@ export default function ReviewDetails({
     productId:string;
 };
 }) {
+  const random = getRandomInt(2);
+  if(random === 1)
+  {
+    throw new Error ("Error loading review");
+  }
     if(parseInt(params.reviewId )> 100)
     {
         notFound();
